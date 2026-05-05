3df36a74106ddb917fcf3655d5134731bf597764
viktorr245 <81569990+viktorr245@users.noreply.github.com>
2026-05-05 14:05:36 +0200

    Clean up build and mixin metadata

--- src/main/java/me/cortex/nvidium/mixin/minecraft/MixinWindow.java
+++ src/main/java/me/cortex/nvidium/mixin/minecraft/MixinWindow.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(Window.class)
 public class MixinWindow {
 
-    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/GL;createCapabilities()Lorg/lwjgl/opengl/GLCapabilities;", shift = At.Shift.AFTER))
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lorg/lwjgl/opengl/GL;createCapabilities()Lorg/lwjgl/opengl/GLCapabilities;", shift = At.Shift.AFTER, remap = false))
     private void init(WindowEventHandler eventHandler, MonitorTracker monitorTracker, WindowSettings settings, String videoMode, String title, CallbackInfo ci) {
         Nvidium.checkSystemIsCapable();
     }
